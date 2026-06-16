275d3c22efa3f5d8deaad394cbd849e45f7cbfe4
rohit-dhok <rohitdhok.dev@gmail.com>
2026-06-16 20:06:20 +0530

    chore: initial portfolio setup

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Navbar from "./components/Navbar"
+import Contact from "./sections/Contact"
+import Footer from "./sections/Footer"
+import Hero from "./sections/Hero"
+import Projects from "./sections/Projects"
+import Skills from "./sections/Skills"
+
+
+function App() {
+  
+  return (
+    <>
+      <Navbar/>
+      <Hero/>
+      <About/>
+      <Skills/>
+      <Projects/>
+      <Contact/>
+      <Footer/>
+    </>
+  )
+      
+}
+
+export default App
