ab3b08ca6b32cb4d44e42343c6dc809edc317228
rohit-dhok <rohitdhok.dev@gmail.com>
2026-06-20 09:32:46 +0530

    fix: add mobile responsive behaviour

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,24 @@
-import { Link } from "react-scroll"
+import { useState } from "react";
+import { Link } from "react-scroll";
+import { Menu, X } from "lucide-react";
 
 function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <nav className="navbar">
       <div className="logo">
         Rohit
       </div>
-
-      <ul className="nav-links">
-        <li><Link to="about" smooth={true} offset={-120} duration={500}>About</Link></li>
-        <li><Link to="skills" smooth={true} offset={-100} duration={500}>Skills</Link></li>
-        <li><Link to="projects" smooth={true} offset={-100} duration={500}>Projects</Link></li>
-        <li><Link to="contact" smooth={true} offset={-70} duration={500}>Contact</Link></li>
+      <div className="hamburger-icon">
+        <button onClick={() => setIsOpen(!isOpen)}>
+          {isOpen ? <X className="hamBtn" size={24}/> : <Menu className="hamBtn" size={24}/>}
+        </button>
+      </div>
+      <ul className={isOpen ? "nav-links open" : "nav-links"}>
+        <li><Link to="about" smooth={true} offset={-120} duration={500} onClick={() => setIsOpen(false)}>About</Link></li>
+        <li><Link to="skills" smooth={true} offset={-100} duration={500} onClick={() => setIsOpen(false)}>Skills</Link></li>
+        <li><Link to="projects" smooth={true} offset={-100} duration={500} onClick={() => setIsOpen(false)}>Projects</Link></li>
+        <li><Link to="contact" smooth={true} offset={-70} duration={500} onClick={() => setIsOpen(false)}>Contact</Link></li>
       </ul>
     </nav>
   )
